612cc1f2c092238c32b5c830e467fe324148b4df
tkjelds <tkjelds385@gmail.com>
2026-07-09 00:06:07 +0200

    Added toggleterm keybinds

--- lua/kickstart/plugins/toggleterm.lua
+++ lua/kickstart/plugins/toggleterm.lua
@@ -1,12 +1,46 @@
-vim.pack.add({
-  "https://github.com/akinsho/toggleterm.nvim",
+vim.pack.add {
+  'https://github.com/akinsho/toggleterm.nvim',
+  'https://github.com/nvim-lua/plenary.nvim',
+}
+local map = vim.keymap.set
+
+require('toggleterm').setup {
+  size = 20,
+  open_mapping = [[<c-\>]], -- optional fallback
+  direction = 'float',
+  float_opts = {
+    border = 'curved',
+  },
+}
+
+map(
+  'n',
+  '<leader>gg',
+  function()
+    require('toggleterm.terminal').Terminal
+      :new({
+        cmd = 'lazygit',
+        direction = 'float',
+      })
+      :toggle()
+  end,
+  {
+    desc = 'Open lazygit',
+  }
+)
+
+map('n', '<leader>tt', '<cmd>ToggleTerm<cr>', {
+  desc = 'Toggle terminal',
 })
 
-vim.keymap.set("n", "<leader>gg", function()
-  require("toggleterm.terminal").Terminal:new({
-    cmd = "lazygit",
-    direction = "float",
-  }):toggle()
-end, {
-  desc = "Open lazygit",
+map('n', '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', {
+  desc = 'Floating terminal',
+})
+
+map('n', '<leader>th', '<cmd>ToggleTerm direction=horizontal<cr>', {
+  desc = 'Horizontal terminal',
+})
+
+map('n', '<leader>tv', '<cmd>ToggleTerm direction=vertical<cr>', {
+  desc = 'Vertical terminal',
 })
